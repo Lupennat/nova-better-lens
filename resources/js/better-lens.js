1733a9cae5482bc712c15e8c48ab1663a4a62c8b
Claudio Pennati <claudio.pennati@gmail.com>
2023-12-09 18:22:22 +0100

    changed folder tree fixed prettier

--- resources/js/better-lens.js
+++ resources/js/better-lens.js
@@ -1,18 +1,18 @@
-import LensPage from "./pages/Lens";
-import LensView from "./views/Lens";
-import HasManyLensField from "./components/HasManyLensField";
-import HasManyThroughLensField from "./components/HasManyThroughLensField";
-import BelongsToManyLensField from "./components/BelongsToManyLensField";
-import MorphToManyLensField from "./components/MorphToManyLensField";
-import LensField from "./components/LensField";
+import LensPage from './pages/Lens';
+import LensView from './views/Lens';
+import HasManyLensField from './fields/HasManyLensField';
+import HasManyThroughLensField from './fields/HasManyThroughLensField';
+import BelongsToManyLensField from './fields/BelongsToManyLensField';
+import MorphToManyLensField from './fields/MorphToManyLensField';
+import LensField from './fields/LensField';
 
-Nova.inertia("Nova.BetterLens", LensPage);
+Nova.inertia('Nova.BetterLens', LensPage);
 
 Nova.booting((app, store) => {
-  app.component("ResourceBetterLens", LensView);
-  app.component("detail-has-many-lens-field", HasManyLensField);
-  app.component("detail-has-many-through-lens-field", HasManyThroughLensField);
-  app.component("detail-belongs-to-many-lens-field", BelongsToManyLensField);
-  app.component("detail-morph-to-many-lens-field", MorphToManyLensField);
-  app.component("detail-lens-field", LensField);
+    app.component('ResourceBetterLens', LensView);
+    app.component('detail-has-many-lens-field', HasManyLensField);
+    app.component('detail-has-many-through-lens-field', HasManyThroughLensField);
+    app.component('detail-belongs-to-many-lens-field', BelongsToManyLensField);
+    app.component('detail-morph-to-many-lens-field', MorphToManyLensField);
+    app.component('detail-lens-field', LensField);
 });
---
+++ resources/js/fields/HasManyLensField.vue
@@ -0,0 +1,37 @@
+<template>
+    <ResourceBetterLens
+        :field="field"
+        :resource-name="field.resourceName"
+        :via-resource="resourceName"
+        :via-resource-id="resourceId"
+        :via-relationship="field.hasManyRelationship"
+        :create-link-parameters="field.createLinkParameters"
+        relationship-type="hasMany"
+        @actionExecuted="actionExecuted"
+        :load-cards="false"
+        :initialPerPage="field.perPage || 5"
+        :should-override-meta="false"
+        :can-collapse="true"
+        :lens="field.lensName"
+        :is-authorized-to-create="field.isAuthorizedToCreate"
+        :searchable="field.searchable"
+        :show-pagination="field.showPagination"
+    />
+</template>
+
+<script>
+    export default {
+        emits: ['actionExecuted'],
+
+        props: ['resourceName', 'resourceId', 'field'],
+
+        methods: {
+            /**
+             * Handle the actionExecuted event and pass it up the chain.
+             */
+            actionExecuted() {
+                this.$emit('actionExecuted');
+            },
+        },
+    };
+</script>
